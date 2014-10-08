0e94c1d061fae96f30130b30c95bbc66aa67bf26
Nick Zahn <nic.z@me.com>
2014-10-08 15:40:42 +0100

    Add multi_line_text_to_html

--- src/StrPlus.php
+++ src/StrPlus.php
@@ -23,7 +23,7 @@ class StrPlus {
 	 */
 	public static function single_line($string) {
 		$string = self::replace_control_chars(' ', $string);
-		while (strpos($string, '  ') !== FALSE) {
+		while (strpos($string, '  ') !== false) {
 			$string = str_replace('  ', ' ', $string);
 		}
 		return trim($string);
@@ -42,6 +42,17 @@ class StrPlus {
 		$string = html_entity_decode($string);
 		$string = str_replace("\xc2\xa0", ' ', $string); // Replce UTF-8 version of &nbsp; (C2 A0) with normal space
 		return self::single_line($string);
+	}
+
+	/**
+	 * Converts a multi-line plain text string to a multi-line HTML string
+	 * where line breaks become <br> tags.
+	 *
+	 * @param string
+	 * @return string
+	 */
+	public static function multi_line_text_to_html($string) {
+		return nl2br(htmlspecialchars($string), false);
 	}
 
 }
